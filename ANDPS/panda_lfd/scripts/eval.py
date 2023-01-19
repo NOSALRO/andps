bf45b2f6626e8f15b2186d5d177d7db373b4f2c8
DionisTotsila <st1067532@ceid.upatras.gr>
2023-01-19 22:27:54 +0200

    force perturbations + save data

--- ANDPS/panda_lfd/scripts/eval.py
+++ ANDPS/panda_lfd/scripts/eval.py
@@ -4,14 +4,20 @@ import dartpy
 from utils import net_rd as andps, simple_nn, torch, damped_pseudoinverse, PIDTask
 # from generate_datasets import get_joints_state, get_eef_state, set_initial_pos
 # Get Joint Positions (Angles)
+
+
 def get_joints_state(robot):
     return robot.positions()[:7]
 
 # get end effector position (xyz)
+
+
 def get_eef_state(robot):
     return robot.body_pose(eef_link_name).translation()
 
 # Set Franka to Initial Configuration
+
+
 def set_initial_pos(robot):
     robot.reset()
     positions = robot.positions()
@@ -22,14 +28,16 @@ def set_initial_pos(robot):
     robot.set_positions(positions)
 
 
-enable_graphics = True # set to False to run without graphics
+enable_graphics = True  # set to False to run without graphics
 
-experiment = "eef"
-# experiment = "joint"
+# experiment = "eef"
+experiment = "joint"
 
-policy = "ANDPS"
-# policy = "NN"
+# policy = "ANDPS"
+policy = "NN"
 
+# set to true to apply force perturbation (push the end effector)
+apply_force = False
 # read dataset
 dataset = np.load("data/spiral_" + experiment + "_space.npy")
 if experiment == "eef":
@@ -39,15 +47,18 @@ elif experiment == "joint":
     np_X = dataset[:, :7]
     np_Y = dataset[:, -7:]
 
-target = np_X[-1, :]
+target_eef = np.load("data/spiral_eef_space.npy")[-1, :3]
+target = np_X[-1, :].copy()
 dims = np_X.shape[1]
+
 if policy == "ANDPS":
     model = andps(dims, 6, target)
 
 elif policy == "NN":
     model = simple_nn(dims)
 
-model.load_state_dict(torch.load("models/"+policy+"_spiral_" + experiment + "_1000.pt"))
+model.load_state_dict(torch.load(
+    "models/"+policy+"_spiral_" + experiment + "_1000.pt"))
 model.eval()
 
 
@@ -59,7 +70,7 @@ simu.set_collision_detector("fcl")
 simu.add_checkerboard_floor()
 
 # RobotDART Graphics
-if(enable_graphics):
+if (enable_graphics):
     gconfig = rd.gui.Graphics.default_configuration()
     gconfig.width = 640
     gconfig.height = 480
@@ -69,7 +80,6 @@ if(enable_graphics):
     graphics.camera().record(True)
     simu.enable_status_bar(enable=False)
     graphics.record_video("media/videos/spiral_eval_"+experiment+".mp4")
-
 
 
 # RobotDART Robot
@@ -82,15 +92,14 @@ simu.add_robot(robot)
 
 set_initial_pos(robot)
 if experiment == "joint":
-    # Initialize datasets
-    joints_state_cur = get_joints_state(robot)
-    joints_dataset = np.array(joints_state_cur)
     get_state = get_joints_state
 elif experiment == "eef":
-    eef_state_cur = get_eef_state(robot)
-    eef_trajectory = np.array(eef_state_cur)
     get_state = get_eef_state
-
+# Initialize datasets
+joints_state_cur = get_joints_state(robot)
+joints_dataset = np.array(joints_state_cur)
+eef_state_cur = get_eef_state(robot)
+eef_trajectory = np.array(eef_state_cur)
 
 # Controller for fixed eef orientation
 Kp = np.array([20., 20., 20., 10., 10., 10.])
@@ -101,48 +110,79 @@ controller.set_target(robot.body_pose(eef_link_name))
 
 #  visualize target
 tf_target = dartpy.math.Isometry3()
-tf_target.set_translation(target)
-simu.add_visual_robot(rd.Robot.create_ellipsoid([0.05, 0.05, 0.05], tf_target, "fix",  color=[0, 1, 0., 1], ellipsoid_name='target'))
+tf_target.set_translation(target_eef)
+simu.add_visual_robot(rd.Robot.create_ellipsoid(
+    [0.05, 0.05, 0.05], tf_target, "fix",  color=[0, 1, 0., 1], ellipsoid_name='target'))
 
 # helper variables
 timestep_counter = 0
 t = 0
 leave_trace = 0
-continue_simu = True
-x_cur = get_eef_state(robot)
+push_flag = False
+x_cur = get_state(robot)
+
+
+# store time of force perturbation
+time_force = []
+force_push = ""
 with torch.no_grad():
-    while(np.linalg.norm(get_state(robot)-target)>1e-2):
+    while (t<10):
         t += dt
         update = False
         if simu.scheduler().schedule(control_freq):
             update = True
-            if(experiment == "eef"):
+            if (experiment == "eef"):
                 vel_rot = controller.update(robot.body_pose(eef_link_name))[0][:3]
-                vel_xyz = model(torch.Tensor(x_cur).view(-1,dims)).detach().numpy().copy()
-                vel = [vel_rot[0], vel_rot[1], vel_rot[2], vel_xyz[0,0], vel_xyz[0,1], vel_xyz[0,2]]
+                vel_xyz = model(torch.Tensor(x_cur).view(-1, dims)).detach().numpy().copy()
+                vel = np.concatenate((vel_rot, vel_xyz.reshape(3,)))
                 # Jacobian pseudo-inverse
                 jac_pinv = damped_pseudoinverse(robot.jacobian(eef_link_name))
                 # Compute joint commands
                 cmd = jac_pinv @ vel
-            elif(experiment == "joint"):
-                joints_vel = model(torch.Tensor(get_joints_state(robot)).view(-1,dims)).detach().numpy()
+            elif (experiment == "joint"):
+                joints_vel = model(torch.Tensor(x_cur).view(-1, dims)).detach().numpy()
                 cmd = np.append(joints_vel, np.zeros(2))
-
 
             robot.set_commands(cmd)
 
-            # Add spheres to visualize the trajectory in the GUI
-            if(enable_graphics):
-                leave_trace = leave_trace + 1
-                if leave_trace == 1 or leave_trace % 10 == 0:
-                    simu.add_visual_robot(rd.Robot.create_ellipsoid([0.05, 0.05, 0.05], robot.body_pose(
-                    eef_link_name), "fix",  color=[1, 0.647, 0., 1], ellipsoid_name=str(leave_trace)))
+            # external force application
+            if apply_force:
+                force_push = "_force_push"
+                if leave_trace == 0:
+                    robot.set_external_force("panda_ee", [120, 0, 0])
+                    push_flag = True
+                    print("Push Robot")
+                    time_force.append(t)
+                if leave_trace == 15:
+                    robot.set_external_force("panda_ee", [0, 0, 0])
+                    push_flag = False
+                if leave_trace == 500:
+                    push_flag = True
+                    robot.set_external_force("panda_ee", [-55,-40,-55])
+                    time_force.append(t)
+                    print("Push Robot")
+                if leave_trace == 510:
+                    push_flag = False
+                    robot.set_external_force("panda_ee", [0, 0, 0])
 
-        if(update):
-            x_cur = get_state(robot)
+            # Add spheres to visualize the trajectory in the GUI
+            leave_trace = leave_trace + 1
+            if (enable_graphics):
+                if (leave_trace == 1) or (leave_trace % 10 == 0) and push_flag == False:
+                    simu.add_visual_robot(rd.Robot.create_ellipsoid([0.05, 0.05, 0.05], robot.body_pose(
+                        eef_link_name), "fix",  color=[0.9, 0.9, 0., 0.8], ellipsoid_name=str(leave_trace)))
+                elif (leave_trace == 1) or (leave_trace % 10 == 0) and push_flag == True:
+                    simu.add_visual_robot(rd.Robot.create_ellipsoid([0.05, 0.05, 0.05], robot.body_pose(
+                        eef_link_name), "fix",  color=[1, 0., 0., 0.8], ellipsoid_name=str(leave_trace)))
+
 
         simu.step_world()
-        # if update:
-        #     joints_state_cur = get_joints_state(robot)
-        #     joints_dataset = np.vstack((joints_dataset, joints_state_cur))
-        #     eef_trajectory = np.vstack((eef_trajectory, robot.body_pose(eef_link_name).translation()))
+        if update:
+            x_cur = get_state(robot)
+            joints_dataset = np.vstack((joints_dataset, get_joints_state(robot)))
+            eef_trajectory = np.vstack((eef_trajectory, robot.body_pose(eef_link_name).translation()))
+
+
+
+# store data for plotting
+np.savez("data/"+policy+"_spiral_eval_"+experiment+force_push+".npz", joints_dataset=joints_dataset, eef_trajectory = eef_trajectory, time_force = time_force, target = target, end_time = t, target_eef = target)
